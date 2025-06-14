7cb4c9220a11e26ba1fe960db5d233c0dfd0d615
Kundan Prajapati <kundankumarp82@gmail.com>
2025-06-14 02:46:50 +0000

    container config done

---
+++ variables and datatypes/datatypes.js
@@ -0,0 +1,19 @@
+// datatypes in javascript
+
+
+let name ="kundan";      // string
+console.log(typeof name);
+
+let age = 20;            // Number, bigInt
+console.log(typeof age);
+
+let isStudent = true;    // boolean
+console.log(typeof isStudent);
+
+let x = null;             //null
+console.log(typeof x);    // object
+
+let email_id ;           //undefined   (value not assigned)
+console.log(typeof email_id);
+
+// symbol -> uniqness define
